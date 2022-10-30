5e19ab1dd1e609d5cbd7b54fe4d86fabf876440f
somedamnauthor <sreeshnelp@gmail.com>
2022-10-30 20:52:52 +0100

    Added Hadoop setup steps, removed unnecessary files

--- hadoop_install/conf/config.sh
+++
@@ -1,4 +0,0 @@
-HADOOP_USER_PASSWORD=hadoop
-HADOOP_ORIGIN=https://www-eu.apache.org/dist/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-HADOOP_PARENT_DIR=/usr/local
